ac3cb55cde2e46dfe1e69349f5d67ff69a73c737
Anhagu <leejhsam330@gmail.com>
2026-06-29 02:31:03 +0900

    상품 페이징 및 정렬

--- src/main/java/com/anhagu/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/anhagu/myselectshop/repository/ProductRepository.java
@@ -2,10 +2,12 @@ package com.anhagu.myselectshop.repository;
 
 import com.anhagu.myselectshop.entity.Product;
 import com.anhagu.myselectshop.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findAllByUser(User user);
+    Page<Product> findAllByUser(User user, Pageable pageable);
 }
