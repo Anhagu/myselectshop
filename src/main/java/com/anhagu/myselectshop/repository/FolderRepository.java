4ab4057caa99592db69576e2f1263f46d3d091de
Anhagu <leejhsam330@gmail.com>
2026-06-29 02:47:33 +0900

    상품 폴더 설계

---
+++ src/main/java/com/anhagu/myselectshop/repository/FolderRepository.java
@@ -0,0 +1,7 @@
+package com.anhagu.myselectshop.repository;
+
+import com.anhagu.myselectshop.entity.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FolderRepository extends JpaRepository<Folder, Long> {
+}
---
+++ src/main/java/com/anhagu/myselectshop/repository/ProductFolderRepository.java
@@ -0,0 +1,7 @@
+package com.anhagu.myselectshop.repository;
+
+import com.anhagu.myselectshop.entity.ProductFolder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductFolderRepository extends JpaRepository<ProductFolder, Long> {
+}
