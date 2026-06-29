9664815538d51dc3f1a4f2cfc4b204a423b2e6c0
Anhagu <leejhsam330@gmail.com>
2026-06-29 11:13:55 +0900

    폴더 별 관심상품 조회 구현

--- src/main/java/com/anhagu/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/anhagu/myselectshop/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.anhagu.myselectshop.repository;
 
+import com.anhagu.myselectshop.dto.ProductResponseDto;
 import com.anhagu.myselectshop.entity.Product;
 import com.anhagu.myselectshop.entity.User;
 import org.springframework.data.domain.Page;
@@ -10,4 +11,6 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAllByUser(User user, Pageable pageable);
+
+    Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
 }
