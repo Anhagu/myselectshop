ddfa82dcac93de1bf5506c09d3cca7c281f4ecb1
Anhagu <leejhsam330@gmail.com>
2026-06-30 02:11:29 +0900

    Spring AOP 적용

--- src/main/java/com/anhagu/myselectshop/controller/ProductController.java
+++ src/main/java/com/anhagu/myselectshop/controller/ProductController.java
@@ -1,12 +1,8 @@
 package com.anhagu.myselectshop.controller;
 
-import com.anhagu.myselectshop.dto.FolderRequestDto;
 import com.anhagu.myselectshop.dto.ProductMypriceRequestDto;
 import com.anhagu.myselectshop.dto.ProductRequestDto;
 import com.anhagu.myselectshop.dto.ProductResponseDto;
-import com.anhagu.myselectshop.entity.ApiUseTime;
-import com.anhagu.myselectshop.entity.User;
-import com.anhagu.myselectshop.repository.ApiUseTimeRepository;
 import com.anhagu.myselectshop.security.UserDetailsImpl;
 import com.anhagu.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
@@ -14,47 +10,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class ProductController {
 
     private final ProductService productService;
-    private final ApiUseTimeRepository apiUseTimeRepository;
 
     @PostMapping("/products")
     public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        // 측정 시작 시간
-        long startTime = System.currentTimeMillis();
-
-        try {
-            // 응답 보내기
-            return productService.createProduct(requestDto, userDetails.getUser());
-        } finally {
-            // 측정 종료 시간
-            long endTime = System.currentTimeMillis();
-            // 수행시간 = 종료 시간 - 시작 시간
-            long runTime = endTime - startTime;
-
-            // 로그인 회원 정보
-            User loginUser = userDetails.getUser();
-
-            // API 사용시간 및 DB 에 기록
-            ApiUseTime apiUseTime = apiUseTimeRepository.findByUser(loginUser)
-                    .orElse(null);
-            if (apiUseTime == null) {
-                // 로그인 회원의 기록이 없으면
-                apiUseTime = new ApiUseTime(loginUser, runTime);
-            } else {
-                // 로그인 회원의 기록이 이미 있으면
-                apiUseTime.addUseTime(runTime);
-            }
-
-            System.out.println("[API Use Time] Username: " + loginUser.getUsername() + ", Total Time: " + apiUseTime.getTotalTime() + " ms");
-            apiUseTimeRepository.save(apiUseTime);
-        }
+        return productService.createProduct(requestDto, userDetails.getUser());
     }
 
     @PutMapping("/products/{id}")
